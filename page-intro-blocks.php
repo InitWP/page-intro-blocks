b3ad9c646961f381c6fcaecbe6a72b0a096da555
Sjoerd Lohuis <sjoerdlohuis@Sjoerds-iMac.local>
2021-05-30 17:04:12 +0200

    Module Page Intro Blocks

---
+++ page-intro-blocks.php
@@ -0,0 +1,37 @@
+<?php /************************************* Page Intro Blocks *************************************/
+if( have_rows('page_intro_blocks') ): ?>
+	<div class="grid-{{ number }} teaserIntroBlocks grid-equalColumns">
+
+	<?php while( have_rows('page_intro_blocks') ): the_row();
+
+		// Get the page
+		$block_title = get_sub_field('page_intro_block_title');
+		$block_introtext = get_sub_field('page_intro_block_introtext');
+		$block_pagelink = get_sub_field('page_intro_block_page_link');
+
+		if ($block_title && $block_introtext && $block_pagelink) {
+		?>
+			<div class="teaserIntroBlock">
+				<a href="<?php echo $block_pagelink; ?>" class="teaserIntroBlock--inner">
+					{{ title }}
+					<h2 class="teaserIntroBlock--title"><?php echo $block_title; ?></h2>
+					{{ /title }}
+					<div class="teaserIntroBlock--content">
+						<?php echo $block_introtext; ?>
+					</div>
+					{{ readmore }}
+					<div class="readmore">
+						<?php echo 'Read more > (make global)' ?>
+					</div>
+					{{ /readmore }}
+				</a>
+			</div>
+			<?php
+		}
+
+	endwhile; ?>
+
+	</div>
+<?php
+endif;
+/************************************* /Page Intro Blocks *************************************/ ?>
